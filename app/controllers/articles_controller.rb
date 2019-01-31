38bf240737804e0f2d8cdcffdc0f7f04d84452ae
kariem Gerges <kiko1086@gmail.com>
2019-01-31 15:29:58 -0500

    get destroy and paticales

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -36,6 +36,13 @@ def update
     end
 end
 
+def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+  flash[:notice] = "article was sucesfully deleted"
+  redirect_to articles_path
+end
+
   private
     def article_params
     params.require(:article).permit(:title, :discreption)
